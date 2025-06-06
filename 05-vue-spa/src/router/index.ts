d8a8d93672981de5b31c489939f4f6373595d5fe
Hugo <hugo_mendoza_s@outlook.com>
2025-06-06 17:11:12 -0500

    section spa app

---
+++ 05-vue-spa/src/router/index.ts
@@ -0,0 +1,43 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import HomePage from '@/modules/landing/pages/HomePage.vue'
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: '/',
+      name: 'landing',
+      component: () => import('@/modules/landing/layouts/LandingLayout.vue'),
+      children: [
+        {
+          path: '/',
+          name: 'home',
+          component: HomePage,
+        },
+        {
+          path: '/features',
+          name: 'features',
+          component: () => import('@/modules/landing/pages/FeaturesPage.vue'),
+        },
+        {
+          path: '/pricing',
+          name: 'pricing',
+          component: () => import('@/modules/landing/pages/PricingPage.vue'),
+        },
+        {
+          path: '/contact',
+          name: 'contact',
+          component: () => import('@/modules/landing/pages/ContactPage.vue'),
+        },
+      ],
+    },
+
+    //Auth
+    {
+      path: '/auth',
+      component: () => import('@/modules/auth/pages/LoginPage.vue'),
+    },
+  ],
+})
+
+export default router
